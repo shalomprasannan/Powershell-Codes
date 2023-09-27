2afd662fc8e181b1a719a5412ff81a2414c639d5
shalomprasannan <34344441+shalomprasannan@users.noreply.github.com>
2023-09-27 16:18:31 +0800

    Add files via upload

---
+++ Healthed.ps1
@@ -0,0 +1,35 @@
+﻿$machines = Get-content .\machines.txt
+$color="white"
+
+$Array=@()
+foreach($machine in $machines){
+    if(Test-connection $machine -Count 1 -Quiet){
+        $reachable =  "Reachable"
+        try{
+            $data = Invoke-command -ComputerName "$machine.m1.local" -ErrorAction SilentlyContinue -ScriptBlock {
+                (Get-wmiobject -Class Win32_OperatingSystem).LastBootUpTime }
+            $now = Get-Date
+            $timeDiff = $now - [System.Management.ManagementDateTimeConverter]::ToDateTime($data)
+            $data = $timeDiff.Days
+        }
+        catch{
+            $data = "No winRM"
+        }
+    
+    }
+    else{
+        $reachable ="unReachable"
+    }
+    $object = [pscustomobject]@{
+    Machine = $machine
+    Reachable = $reachable
+    BootTime = $data
+    }
+    $object
+    $data=""
+    $machine=""
+    $reachable=""
+    $Array+=$object
+
+}
+$Array|Export-csv -path .\output.txt
